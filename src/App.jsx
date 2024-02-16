b790affa8fe206177e80611d245487a686a77f16
vinnugollakoti <vinay.reddy@kalvium.community>
2024-02-16 14:08:53 +0530

    added and coded

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ import './App.css'
 import data from './data'
 function App() {
   const [state, setstate] = useState("")
-  let refe=useRef()
+  let reference=useRef()
   document.body.onkeydown=(e)=>{
     if(e.key=="Escape"){
-    refe.current.style.display="none"
+    reference.current.style.display="none"
     console.log("Escape")
     }}
   return (
     <div>
       <center>
       <input onChange={(e)=>{setstate(e.target.value)
-      refe.current.style.display="block"
+      reference.current.style.display="block"
       }}
        />
-      <div ref={refe}>
-        {data.filter((i)=>{return state!==""?i.name.startsWith(state):0}).map((i,ind)=>
-        <h2 key={ind} >{i.name}</h2>)
+      <div ref={reference}>
+      {data.filter((i)=>{return state!==""?i.name.toLowerCase().startsWith(state.toLowerCase()):0}).map((i,id)=>
+        <h2 key={id} >{i.name}</h2>)
         }
       </div></center>
     </div>
